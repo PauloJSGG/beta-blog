6f99cb73254c55f35f939d7f286c2d957c0a14aa
Rails Udemy Course <paulojsgg@gmail.com>
2019-04-24 15:58:58 +0100

    added pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -23,7 +23,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def update
+def update
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
